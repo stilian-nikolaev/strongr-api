120d20638758e11b9c4821fe5e2a6265df66c53c
stilian-nikolaev <74594156+stilian-nikolaev@users.noreply.github.com>
2022-05-12 09:02:32 +0300

    when creating exercise we add exercise id to corresponding workout

--- services/exerciseService.js
+++ services/exerciseService.js
@@ -8,16 +8,20 @@ module.exports = {
     getOne(id) {
         return Exercise.findById(id);
     },
-    create(reqBody) {
-        const workout = workoutService.findById(reqBody.workoutId);
+    async create(reqBody) {
+        const workout = await workoutService.getOne(reqBody.workoutId);
 
         if (!workout) throw { message: 'There is no workout with the corresponding ID' }
 
+        //TODO: validate input
         const exercise = new Exercise({ title: reqBody.title, sets: reqBody.sets, workoutId: reqBody.workoutId })
-        
+
+        await workoutService.addExercise(reqBody.workoutId, exercise._id)
+
         return exercise.save();
     },
     edit(id, reqBody) {
+        //TODO: validate input
         return Exercise.updateOne({ id }, reqBody);
     },
     delete(id) {
--- services/workoutService.js
+++ services/workoutService.js
@@ -8,14 +8,27 @@ module.exports = {
         return Workout.findById(id);
     },
     create(reqBody) {
+        //TODO: validate input
+
         const workout = new Workout({ title: reqBody.title, exercises: reqBody.exercises })
         return workout.save();
     },
     edit(id, reqBody) {
+        //TODO: validate input
+
         return Workout.updateOne({ id }, reqBody);
     },
     delete(id) {
         return Workout.deleteOne({ id });
-    }
+    },
+    async addExercise(workoutId, exerciseId) {
+        const workout = await Workout.findById(workoutId);
+
+        const currentExercises = workout.exercises;
+
+        currentExercises.push(exerciseId);
+
+        return Workout.updateOne({_id: workoutId}, {exercises: currentExercises})
+    }   
 
 }
