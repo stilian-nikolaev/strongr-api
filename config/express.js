323bb5d06d3670544135cf18b0fe92407e272511
stilian-nikolaev <74594156+stilian-nikolaev@users.noreply.github.com>
2022-05-12 16:33:22 +0300

    configure cors

--- config/express.js
+++ config/express.js
@@ -3,5 +3,12 @@ const express = require('express');
 module.exports = (app) => {
     app.use(express.json());
 
+    //configure cors used for dev
+    app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "http://localhost:3000"); 
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
+
     return app;
 }
