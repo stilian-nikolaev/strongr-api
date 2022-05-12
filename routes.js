836163b138dc039eadd9316a3d12595fd861ee54
stilian-nikolaev <74594156+stilian-nikolaev@users.noreply.github.com>
2022-05-12 08:06:07 +0300

    added exercise controller to routes

--- routes.js
+++ routes.js
@@ -1,8 +1,10 @@
 const { Router } = require('express')
 const router = Router()
 const workoutController = require('./controllers/workoutController')
+const exerciseController = require('./controllers/exerciseController')
 
 router.use('/workouts', workoutController);
+router.use('/exercises', exerciseController);
 router.get('*', (req, res) => {
     res.status(404).send('Not Found');
 });
