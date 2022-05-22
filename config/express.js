88dd9c9df978b96160e81e460481ffc7d218adfc
unknown <stili_nikolaev@abv.bg>
2022-05-22 12:29:57 +0300

    allowed delete method cors

--- config/express.js
+++ config/express.js
@@ -7,6 +7,7 @@ module.exports = (app) => {
     app.use(function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "http://localhost:3000"); 
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
         next();
     });
 
