0784564a9a89e49a82a04af2e7499093e46080d3
stilian-nikolaev <74594156+stilian-nikolaev@users.noreply.github.com>
2022-07-02 13:35:37 +0300

    added cors lib

--- config/express.js
+++ config/express.js
@@ -1,14 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
 module.exports = (app) => {
     app.use(express.json());
 
-    app.use(function (req, res, next) {
-        res.header("Access-Control-Allow-Origin", "http://localhost:3000"); 
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS");
-        next();
-    });
+    app.use(cors());
 
     return app;
 }
